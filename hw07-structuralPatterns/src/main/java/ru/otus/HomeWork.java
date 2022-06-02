79a6086543466637ed982791f0675d7c477983c9
Sergey Fedechkin <s.fede4kin@yandex.ru>
2022-06-02 19:57:54 +0300

    hw07 to-do 1 and 2

--- hw07-structuralPatterns/src/main/java/ru/otus/HomeWork.java
+++ hw07-structuralPatterns/src/main/java/ru/otus/HomeWork.java
@@ -1,5 +1,17 @@
 package ru.otus;
 
+import ru.otus.handler.ComplexProcessor;
+import ru.otus.listener.ListenerPrinterConsole;
+import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+import ru.otus.processor.LoggerProcessor;
+import ru.otus.processor.ProcessorConcatFields;
+import ru.otus.processor.ProcessorUpperField10;
+import ru.otus.processor.homework.ProcessorSwitchField11Field12;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeWork {
 
     /*
@@ -20,5 +32,36 @@ public class HomeWork {
            по аналогии с Demo.class
            из элеменов "to do" создать new ComplexProcessor и обработать сообщение
          */
+
+        var processors = List.of(new ProcessorConcatFields(),
+                new LoggerProcessor(new ProcessorUpperField10()),
+                new LoggerProcessor(new ProcessorSwitchField11Field12()));
+
+        var complexProcessor = new ComplexProcessor(processors, ex -> {});
+        var listenerPrinter = new ListenerPrinterConsole();
+        complexProcessor.addListener(listenerPrinter);
+
+        var objMsg = new ObjectForMessage();
+        var strList = new ArrayList<String>();
+        strList.add("field13_1");
+        strList.add("field13_2");
+        objMsg.setData(strList);
+
+        var message = new Message.Builder(1L)
+                .field1("field1")
+                .field2("field2")
+                .field3("field3")
+                .field6("field6")
+                .field10("field10")
+                .field11("field11")
+                .field12("field12")
+                .field13(objMsg)
+                .build();
+
+        var result = complexProcessor.handle(message);
+        System.out.println("result:" + result);
+
+        complexProcessor.removeListener(listenerPrinter);
+
     }
 }
--- hw07-structuralPatterns/src/main/java/ru/otus/model/Message.java
+++ hw07-structuralPatterns/src/main/java/ru/otus/model/Message.java
@@ -15,7 +15,24 @@ public class Message {
 
     //todo: 1. Добавить поля field11 - field13 (для field13 используйте класс ObjectForMessage)
 
-    private Message(long id, String field1, String field2, String field3, String field4, String field5, String field6, String field7, String field8, String field9, String field10) {
+    private final String field11;
+    private final String field12;
+    private final ObjectForMessage field13;
+
+    private Message(long id,
+                    String field1,
+                    String field2,
+                    String field3,
+                    String field4,
+                    String field5,
+                    String field6,
+                    String field7,
+                    String field8,
+                    String field9,
+                    String field10,
+                    String field11,
+                    String field12,
+                    ObjectForMessage field13) {
         this.id = id;
         this.field1 = field1;
         this.field2 = field2;
@@ -27,6 +44,9 @@ public class Message {
         this.field8 = field8;
         this.field9 = field9;
         this.field10 = field10;
+        this.field11 = field11;
+        this.field12 = field12;
+        this.field13 = field13;
     }
 
     public long getId() {
@@ -73,6 +93,18 @@ public class Message {
         return field10;
     }
 
+    public String getField11() {
+        return field11;
+    }
+
+    public String getField12() {
+        return field12;
+    }
+
+    public ObjectForMessage getField13() {
+        return field13;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -89,7 +121,7 @@ public class Message {
     }
 
     public Builder toBuilder() {
-        return new Builder(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10);
+        return new Builder(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10, field11, field12, field13);
     }
 
     @Override
@@ -106,6 +138,9 @@ public class Message {
                 ", field8='" + field8 + '\'' +
                 ", field9='" + field9 + '\'' +
                 ", field10='" + field10 + '\'' +
+                ", field11='" + field11 + '\'' +
+                ", field12='" + field12 + '\'' +
+                ", field13='" + (field13 == null ? "null" : field13.getData().toString()) + '\'' +
                 '}';
     }
 
@@ -121,12 +156,28 @@ public class Message {
         private String field8;
         private String field9;
         private String field10;
+        private String field11;
+        private String field12;
+        private ObjectForMessage field13;
 
         public Builder(long id) {
             this.id = id;
         }
 
-        private Builder(long id, String field1, String field2, String field3, String field4, String field5, String field6, String field7, String field8, String field9, String field10) {
+        private Builder(long id,
+                        String field1,
+                        String field2,
+                        String field3,
+                        String field4,
+                        String field5,
+                        String field6,
+                        String field7,
+                        String field8,
+                        String field9,
+                        String field10,
+                        String field11,
+                        String field12,
+                        ObjectForMessage field13) {
             this.id = id;
             this.field1 = field1;
             this.field2 = field2;
@@ -138,6 +189,9 @@ public class Message {
             this.field8 = field8;
             this.field9 = field9;
             this.field10 = field10;
+            this.field11 = field11;
+            this.field12 = field12;
+            this.field13 = field13;
         }
 
         public Builder field1(String field1) {
@@ -190,8 +244,23 @@ public class Message {
             return this;
         }
 
+        public Builder field11(String field11) {
+            this.field11 = field11;
+            return this;
+        }
+
+        public Builder field12(String field12) {
+            this.field12 = field12;
+            return this;
+        }
+
+        public Builder field13(ObjectForMessage field13) {
+            this.field13 = field13;
+            return this;
+        }
+
         public Message build() {
-            return new Message(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10);
+            return new Message(id, field1, field2, field3, field4, field5, field6, field7, field8, field9, field10, field11, field12, field13);
         }
     }
 }
