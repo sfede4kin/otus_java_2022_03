2fa429136a7f4d63f4990eeee8a263e33acefee5
Sergey Fedechkin <s.fede4kin@yandex.ru>
2022-06-03 23:58:31 +0300

    hw07 to-do 4

--- hw07-structuralPatterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
+++ hw07-structuralPatterns/src/main/java/ru/otus/listener/homework/HistoryListener.java
@@ -2,17 +2,31 @@ package ru.otus.listener.homework;
 
 import ru.otus.listener.Listener;
 import ru.otus.model.Message;
+import ru.otus.model.ObjectForMessage;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class HistoryListener implements Listener, HistoryReader {
 
+    private final Map<Long, Message> historyMsgMap = new HashMap<>();
+
     @Override
     public void onUpdated(Message msg) {
-        throw new UnsupportedOperationException();
+        try {
+            var msgCloned = msg.toBuilder()
+                                        .field13((ObjectForMessage)msg.getField13().clone())
+                                        .build();
+            historyMsgMap.put(msgCloned.getId(), msgCloned);
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public Optional<Message> findMessageById(long id) {
-        throw new UnsupportedOperationException();
+        return Optional.ofNullable(historyMsgMap.get(id));
     }
 }
--- hw07-structuralPatterns/src/main/java/ru/otus/model/ObjectForMessage.java
+++ hw07-structuralPatterns/src/main/java/ru/otus/model/ObjectForMessage.java
@@ -1,8 +1,9 @@
 package ru.otus.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class ObjectForMessage {
+public class ObjectForMessage implements Cloneable{
     private List<String> data;
 
     public List<String> getData() {
@@ -11,5 +12,12 @@ public class ObjectForMessage {
 
     public void setData(List<String> data) {
         this.data = data;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException{
+        ObjectForMessage objCloned = (ObjectForMessage) super.clone();
+        objCloned.setData(new ArrayList<>(objCloned.getData()));
+        return objCloned;
     }
 }
