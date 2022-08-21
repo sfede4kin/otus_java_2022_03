db436d858b70b665f8070dedd85338ed7e27a665
sfede4kin <s.fede4kin@yandex.ru>
2022-08-21 22:49:52 +0300

    hw16-concurrentCollections #2

--- hw16-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hw16-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -6,12 +6,22 @@ import ru.otus.lib.SensorDataBufferedWriter;
 import ru.otus.api.SensorDataProcessor;
 import ru.otus.api.model.SensorData;
 
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Comparator;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
+
 // Этот класс нужно реализовать
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
+
+    private final ConcurrentNavigableMap<LocalDateTime, SensorData> dataBuffer = new ConcurrentSkipListMap<>(
+                                            Comparator.comparingLong(k -> k.toInstant(ZoneOffset.UTC).toEpochMilli()));
+    private final ReentrantLock lock = new ReentrantLock();
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
@@ -20,19 +30,39 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
 
     @Override
     public void process(SensorData data) {
-    /*
+        dataBuffer.put(data.getMeasurementTime(), data);
         if (dataBuffer.size() >= bufferSize) {
             flush();
         }
-    */
     }
 
     public void flush() {
+
         try {
-            //writer.writeBufferedData(bufferedData);
+            boolean isLockAcquired = lock.tryLock(1, TimeUnit.SECONDS);
+
+            if(isLockAcquired) {
+                var lastEntry = dataBuffer.lastEntry();
+                if (lastEntry == null) {
+                    log.info("lastEntry is null");
+                    return;
+                }
+                log.info("lastKey = {}", lastEntry.getKey());
+
+                var dataBufferHeadMap = dataBuffer.headMap(lastEntry.getKey(), true);
+                log.info("dataBufferHeadMap size = {}", dataBufferHeadMap.size());
+
+                if (dataBufferHeadMap.size() > 0) {
+                    writer.writeBufferedData(dataBufferHeadMap.values().stream().toList());
+                    dataBufferHeadMap.clear();
+                }
+            }
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
+        }finally {
+            lock.unlock();
         }
+
     }
 
     @Override
